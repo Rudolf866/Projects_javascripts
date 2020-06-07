24e2e6f0814bf913da10f4e0e5b5a532d7e619d7
Rudolf <kovalevskiy2017@outlook.com>
2020-06-07 19:23:09 +0300

    'secondCommit'

--- javascript.js
+++ javascript.js
@@ -1,3 +1,19 @@
 function show(){
     alert('You push my button!!!!!');
+    // var popup = window.open('http://vk.com', 'vkontakte', 'width=400, height=400, resizable=yes');
+}
+
+//  let timer=setTimeout(jump,3000);
+// clearTimeout(timerId);
+// setInterval(jump(),3000)
+function date(){
+    let button = document.getElementsByTagName('button');
+    let time = document.getElementById('time');
+    time.innerHTML= new Date().toTimeString();
+
+}
+setInterval(date,1000);
+function jump(){
+    let time = new Date().toTimeString();
+   document.write(time);
 }
